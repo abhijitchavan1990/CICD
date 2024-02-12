9db5c1acb44f13432f9dadc8258b6dd6c7cbfacb
abhijitchavan1990 <abhijitchavan1990@gmail.com>
2024-02-12 23:11:36 +0530

    added test ng class

--- src/test/java/demo/SeleniumDemo.java
+++ src/test/java/demo/SeleniumDemo.java
@@ -2,13 +2,18 @@ package demo;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class SeleniumDemo {
-    public static void main(String[] args) {
+
+    @Test
+    public void startBrowser(){
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("https://facebook.com");
-        System.out.println(driver.getTitle());
+        driver.get("https://opensource-demo.orangehrmlive.com/");
+       System.out.println(driver.getTitle());
+        Assert.assertTrue(driver.getTitle().contains("Orange"), "Title does not match");
         driver.quit();
 
     }
